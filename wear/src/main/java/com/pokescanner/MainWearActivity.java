ecda30d9434c203eedb20a8bc8bf24d3fd3ad112
Julian <julianspaeh@googlemail.com>
2016-09-01 17:44:53 +0200

    Few Updates

--- wear/src/main/java/com/pokescanner/MainWearActivity.java
+++ wear/src/main/java/com/pokescanner/MainWearActivity.java
@@ -13,6 +13,7 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.multidex.MultiDex;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -72,6 +73,7 @@ public class MainWearActivity extends Activity implements SwipeRefreshLayout.OnR
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        MultiDex.install(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_wear);
 
